1504c448be757e87584752725fef37e972903598
aabboudi <abboudi.abdellah@gmail.com>
2024-06-22 14:32:17 +0100

    Changed search icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -64,7 +64,7 @@ export default function TabLayout() {
         options={{
           title: 'Search',
           headerShown: false,
-          tabBarIcon: ({ color }) => <FontAwesome name="search" size={24} color={color} />
+          tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />
         }}
       />
       <Tabs.Screen
