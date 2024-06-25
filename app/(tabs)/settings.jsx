5b98bb87109cb041b62dbdb5934c93c7f9503f5a
aabboudi <abboudi.abdellah@gmail.com>
2024-06-26 00:36:43 +0100

    Refactored settings page

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, View, ScrollView, Text, TouchableOpacity, Switch, Image, Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, View, ScrollView, Text, TouchableOpacity, Switch, Image, Platform, AsyncStorage } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
+import { ChevronButton, SwitchButton } from '@/components/Button';
 import { useColorScheme } from 'react-native';
 import Colors from '@/constants/Colors';
 
 export default function Settings() {
   const [form, setForm] = useState({
     darkMode: false,
-    emailNotifications: true,
-    pushNotifications: false,
   });
-
+  const navigation = useNavigation();
   const colorScheme = useColorScheme();
   const styles = AllStyles(Colors, colorScheme);
 
@@ -30,14 +30,11 @@ export default function Settings() {
 
             <TouchableOpacity
               onPress={() => {
-                // handle onPress
+                navigation.navigate('modal')
               }}
               style={styles.profileAction}>
-              <FeatherIcon
-                color="#fff"
-                name="edit-3"
-                size={15}
-              />
+              <FeatherIcon color="#fff" name="edit-3" size={15}
+            />
             </TouchableOpacity>
           </View>
 
@@ -50,157 +47,16 @@ export default function Settings() {
         <ScrollView>
           <View style={styles.section}>
             <Text style={styles.sectionTitle}>Preferences</Text>
-
-            <TouchableOpacity
-              onPress={() => {
-                // handle onPress
-              }}
-              style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#fe9400' }]}>
-                <FeatherIcon color="#fff" name="globe" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Language</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <FeatherIcon
-                color="#C6C6C6"
-                name="chevron-right"
-                size={20} />
-            </TouchableOpacity>
-
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#007afe' }]}>
-                <FeatherIcon color="#fff" name="moon" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Dark Mode</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <Switch
-                onValueChange={darkMode => setForm({ ...form, darkMode })}
-                value={form.darkMode} />
-            </View>
-
-            <TouchableOpacity
-              onPress={() => {
-                // handle onPress
-              }}
-              style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#32c759' }]}>
-                <FeatherIcon
-                  color="#fff"
-                  name="navigation"
-                  size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Location</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <FeatherIcon
-                color="#C6C6C6"
-                name="chevron-right"
-                size={20} />
-            </TouchableOpacity>
-
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#38C959' }]}>
-                <FeatherIcon
-                  color="#fff"
-                  name="at-sign"
-                  size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Email Notifications</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <Switch
-                onValueChange={emailNotifications =>
-                  setForm({ ...form, emailNotifications })
-                }
-                value={form.emailNotifications} />
-            </View>
-
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#38C959' }]}>
-                <FeatherIcon color="#fff" name="bell" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Push Notifications</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <Switch
-                onValueChange={pushNotifications =>
-                  setForm({ ...form, pushNotifications })
-                }
-                value={form.pushNotifications} />
-            </View>
+            <ChevronButton iconName="globe" label="Language" bgColor='#fe9400' />
+            <SwitchButton iconName="moon" label="Dark Mode" form={form} setForm={setForm} />
+            <ChevronButton iconName="navigation" label="Location" bgColor='#32c759' />
           </View>
 
           <View style={styles.section}>
             <Text style={styles.sectionTitle}>Resources</Text>
-
-            <TouchableOpacity
-              onPress={() => {
-                // handle onPress
-              }}
-              style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#8e8d91' }]}>
-                <FeatherIcon color="#fff" name="flag" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Report Bug</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <FeatherIcon
-                color="#C6C6C6"
-                name="chevron-right"
-                size={20} />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => {
-                // handle onPress
-              }}
-              style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#007afe' }]}>
-                <FeatherIcon color="#fff" name="mail" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Contact Us</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <FeatherIcon
-                color="#C6C6C6"
-                name="chevron-right"
-                size={20} />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => {
-                // handle onPress
-              }}
-              style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: '#32c759' }]}>
-                <FeatherIcon color="#fff" name="star" size={20} />
-              </View>
-
-              <Text style={styles.rowLabel}>Rate in {Platform.OS === 'ios' ? 'App' : 'Play'} Store</Text>
-
-              <View style={styles.rowSpacer} />
-
-              <FeatherIcon
-                color="#C6C6C6"
-                name="chevron-right"
-                size={20} />
-            </TouchableOpacity>
+            <ChevronButton iconName="flag" label="Report Bug" bgColor='#8e8d91' />
+            <ChevronButton iconName="mail" label="Contact Us" bgColor='#007afe' />
+            <ChevronButton iconName="star" label="Rate Us" bgColor='#32c759' />
           </View>
         </ScrollView>
       </View>
@@ -269,38 +125,5 @@ const AllStyles = (Colors, colorScheme) => StyleSheet.create({
     color: '#9e9e9e',
     textTransform: 'uppercase',
     letterSpacing: 1.1,
-  },
-  /** Row */
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: 50,
-    backgroundColor: Colors[colorScheme ?? 'light'].background,
-    borderColor: Colors[colorScheme ?? 'light'].textMuted,
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 12,
-    paddingLeft: 12,
-    paddingRight: 12,
-  },
-  rowIcon: {
-    width: 32,
-    height: 32,
-    borderRadius: 9999,
-    marginRight: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  rowLabel: {
-    fontSize: 17,
-    fontWeight: '400',
-    color: Colors[colorScheme ?? 'light'].text,
-  },
-  rowSpacer: {
-    flexGrow: 1,
-    flexShrink: 1,
-    flexBasis: 0,
   },
 });
---
+++ components/Button.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { Switch } from 'react-native';
+
+import { useColorScheme } from 'react-native';
+import Colors from '@/constants/Colors';
+
+const ChevronButton = ({ onPress, iconName, label, bgColor }) => {
+  const colorScheme = useColorScheme();
+  const styles = AllStyles(Colors, colorScheme);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {onPress}}
+      activeOpacity={0.75}
+      style={styles.row}>
+      <View style={[styles.rowIcon, { backgroundColor: bgColor }]}>
+        <FeatherIcon color="#fff" name={iconName} size={20} />
+      </View>
+
+      <Text style={styles.rowLabel}>{ label }</Text>
+
+      <View style={styles.rowSpacer} />
+
+      <FeatherIcon
+        color="#C6C6C6"
+        name="chevron-right"
+        size={20} />
+    </TouchableOpacity>
+  );
+};
+
+const SwitchButton = ({ iconName, label, form, setForm }) => {
+  const colorScheme = useColorScheme();
+  const styles = AllStyles(Colors, colorScheme);
+
+  return (
+    <View style={styles.row}>
+      <View style={[styles.rowIcon, { backgroundColor: '#007afe' }]}>
+        <FeatherIcon color="#fff" name={iconName} size={20} />
+      </View>
+
+      <Text style={styles.rowLabel}>{label}</Text>
+
+      <View style={styles.rowSpacer} />
+
+      <Switch
+        onValueChange={darkMode => setForm({ ...form, darkMode })}
+        value={form.darkMode}
+        trackColor={{ true: '#81b0ff', false: '#767577' }}
+        thumbColor={form.darkMode ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        // Change colors later
+      />
+    </View>
+  );
+};
+
+export { ChevronButton, SwitchButton };
+
+const AllStyles = (Colors, colorScheme) => StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    backgroundColor: Colors[colorScheme ?? 'light'].background,
+    borderColor: Colors[colorScheme ?? 'light'].textMuted,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  rowIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 9999,
+    marginRight: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: Colors[colorScheme ?? 'light'].text,
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+})
