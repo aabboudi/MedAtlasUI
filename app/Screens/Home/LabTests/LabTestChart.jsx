67cc2af03768e08dd4d0eeda112093eabf7ff307
aabboudi <abboudi.abdellah@gmail.com>
2024-06-27 12:05:05 +0100

    Modified display loop

--- app/Screens/Home/LabTests/LabTestChart.jsx
+++ app/Screens/Home/LabTests/LabTestChart.jsx
@@ -1,98 +1,31 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { useColorScheme } from '@/components/useColorScheme';
-import Colors from '@/constants/Colors';
+import globalStyles from '@/assets/styles/styles';
 
-// const BiologicalChart = ({ route }) => {
 const LabTestChart = () => {
   const route = useRoute();
   const { chart } = route.params;
-  const colorScheme = useColorScheme();
-  const styles = AllStyles(Colors, colorScheme);
+  const styles = globalStyles();
 
   return (
-    <View>
-      <Text style={[styles.header, { color: 'white' }]}>{ chart.title }</Text>
-      <Text style={{ color: 'white' }}>{ chart.parameters[0].name }</Text>
-      <Text style={{ color: 'white' }}>Placeholder</Text>
-      <Text style={{ color: 'white' }}>{ chart.parameters[0].interpretation }</Text>
+    <View style={[styles.container, { alignItems: 'start', padding: 20 }]}>
+      <Text style={[styles.title, {textAlign: 'center'}]}>{chart.title}</Text>
+      <Text style={[styles.pageHeader]}>Normes Biologiques</Text>
 
-      <View style={styles.separator} lightColor="#ccc" darkColor="rgba(255,255,255,0.1)" />
+      {chart.parameters.map((param, index) => (
+        <View key={index}>
+          <Text style={styles.rowTitle}>{param.name}</Text>
+          <Text style={styles.text}>Norme: {param.norme_bio || "Placeholder"}</Text>
+          <Text style={styles.text}>{param.interpretation}</Text>
 
-      <Text style={{ color: 'white' }}>{ chart.parameters[1].name }</Text>
-      <Text style={{ color: 'white' }}>Placeholder</Text>
-      <Text style={{ color: 'white' }}>{ chart.parameters[1].interpretation }</Text>
-
+          {index < chart.parameters.length - 1 && (
+            <View style={styles.separator} lightColor="#ccc" darkColor="rgba(255,255,255,0.1)" />
+          )}
+        </View>
+      ))}
     </View>
   );
 };
-
-const AllStyles = (Colors, colorScheme) => StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    padding: 20,
-    alignItems: 'center',
-  },
-  header: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-    color: Colors[colorScheme ?? 'light'].text,
-  },
-  separator: {
-    marginVertical: 15,
-    height: 1,
-    width: '80%',
-  },
-  inputGroup: {
-    width: '100%', // Full width
-    marginBottom: 15,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 8,
-  },
-  inputLabel: {
-    fontSize: 16,
-    color: Colors[colorScheme ?? 'light'].textMuted,
-    marginBottom: 5,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: '#ccc',
-    backgroundColor: Colors[colorScheme ?? 'light'].background,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 4,
-    fontSize: 16,
-    color: Colors[colorScheme ?? 'light'].textMuted,
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: '#3a445d', // Bootstrap primary blue
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 4,
-  },
-  buttonText: {
-    color: '#fff', // White text on button
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  result: {
-    marginTop: 20,
-    fontSize: 18,
-    color: 'green', // Keep the result in green to highlight success
-    fontWeight: 'bold',
-  },
-});
 
 export default LabTestChart;
